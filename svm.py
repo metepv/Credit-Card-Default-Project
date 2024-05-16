55a5c9a6357161cbb001569440430332d3caa204
Melih Çelik <98035354+metepv@users.noreply.github.com>
2024-05-16 12:19:44 +0300

    for demo

--- svm.py
+++ svm.py
@@ -25,31 +25,9 @@ def data_to_matrix_function():
     predictor_matrix = [] 
     response_vector = []
      
-    for i in range(1,column_size+2):                        #first two line are predictor names.
+    for i in range(1,column_size+2):                    #first two line are predictor names.
         
         column_vector_str = raw_data.readline()
-        """
-        try:
-            first = column_vector_str[0] #if a line starts with a number it will
-            first = int(first)           #it will be added as column to the matrix.
-            
-            column_vector_lst = column_vector_str.split(',') #splitting the line
-                                                             # to add it to a column vector.
-            
-            column_vector_lst[-1] = column_vector_lst[0:-2]  #last element is \n
-                                                             # which is removed.
-            column_vector_int_lst = []                                                 
-            for i2 in range(0,len(column_vector_lst)-1):
-                 column_vector_int_lst.append(int(column_vector_lst[i2]))
-                
-            X.append(column_vector_int_lst)                      
-            
-        except ValueError:
-            continue
-        """
-
-        #due to the integer-float-string conversion the code above generates
-        #matrix with column dimension less than the input value.
 
         if column_vector_str[0].isdigit():
             column_vector_lst = column_vector_str.split(',')
@@ -57,20 +35,14 @@ def data_to_matrix_function():
             response_vector.append(int(column_vector_lst[-1]))
 
             column_vector_float_lst = [1]                                                 
-            for i2 in range(1,len(column_vector_lst)-1): # last value is the y value of the regression.
+            for i2 in range(1,len(column_vector_lst)-1): #last value is the y value of the regression.
                  column_vector_float_lst.append(float(column_vector_lst[i2]))
 
             predictor_matrix.append(column_vector_float_lst)
             
         else:
             continue
-    
-    
-    #NORMALIZATION
-    
-    #print(np.mean(response_vector))
-    # response_vector=random.shuffle(response_vector)
-    # predictor_matrix=random.shuffle(predictor_matrix)
+
     trainset_length=math.floor(len(predictor_matrix)*0.7)
     
     X_test=predictor_matrix[trainset_length:]
@@ -79,76 +51,11 @@ def data_to_matrix_function():
     Y_train=response_vector[:trainset_length]
     return predictor_matrix, response_vector, X_test, X_train, Y_test, Y_train
 
-
-
-# def gradient_descent(X,Y,C,learning_rate,tolerance,iterations):
-    
-#     W=np.zeros(len(X[0]))
-    
-#     for i in range (0,iterations):
-#         gradient=np.zeros(len(X[0]))
-        
-#         for k in range(0,len(X)):
-#             if Y[k] * np.dot(W, X[k])<1:
-
-#                 gradient+=C*Y[k]*X[k]
-
-        
-#         W=W-learning_rate*gradient
-#         if np.all(np.abs(gradient*learning_rate) <= tolerance):   
-#             break
-        
-    
-#     return W
-
-
-
-# def Support_Vector_Machine(X_Train,X_Test,Y_Train,iterations,tolerance,learning_rate,C):
-
-#     Y_Train2=convert_Y(Y_Train)
-
-#     print(np.mean(X_Train))
-#     X_Train = np.insert(X_Train,0,2,axis = 1)
-#     X_Test = np.insert(X_Test,0,2,axis = 1)
-
-
-#     W=gradient_descent(X_Train,Y_Train2,C,learning_rate,tolerance,iterations)
-
-#     training_results=[]
-#     test_results=[]
-    
-    
-
-
-#  #9000000000000000
-#     for X1 in X_Train:
-#         if np.dot(W,X1)>20000:
-#             #print(np.dot(W,X1))
-#             training_results.append(1)
-#         else:
-#             training_results.append(0)
-
-#     for X2 in X_Test:
-#         if np.dot(W,X2)>20000:
-#             test_results.append(1)
-#         else:
-#             test_results.append(0)
-        
-
-#     return training_results,test_results
-
-# iterations=200
-# tolerance=5e-06
-# learning_rate=0.0002
-# C=45
-
 def convert_Y(Y_Train):
     for k in range (len(Y_Train)):
         if Y_Train[k]==0:
             Y_Train[k]=-1
     return Y_Train
-
-
 
 # stochastic gradient descent
 def gradient_descent(iterations, learning_rate, X, Y, C, tolerance):
@@ -170,11 +77,6 @@ def gradient_descent(iterations, learning_rate, X, Y, C, tolerance):
             weights = weights - (gradient * learning_rate)      
     return weights
 
-# def svm_predict(X,W):
-    
-#     return results
-
-
 iterations = 1000
 learning_rate = 0.0002
 tolerance = 5e-07
@@ -189,15 +91,7 @@ def Support_Vector_Machine(X_train, Y_train, X_test, C, iterations, learning_rat
     X_test = np.insert(X_test,0,1,axis = 1)
 
     weights = gradient_descent(iterations, learning_rate, X_train, Y_train, C, tolerance)
-
-
-    # train_results=-1*np.ones(len(X_train))
-    # for Xi_train in range(len(X_train)):
         
-    #     if  np.dot(weights,X_train[Xi_train]) >0:
-    #         train_results[Xi_train]=1
-        
-
     results = -1*np.ones(len(X_test))
     for Xi in range(len(X_test)):
         
@@ -206,8 +100,6 @@ def Support_Vector_Machine(X_train, Y_train, X_test, C, iterations, learning_rat
     return results
     return train_results, results    
   
-
-
 dt = pd.read_csv('UCI_Credit_Card.csv')
  
 dt.columns = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","y"]
@@ -229,16 +121,14 @@ Y_Train=Y[15000:30000]
 Y_Test=Y[15000:30000]
 
 #C=150
-
 #c_list=[1,15,35,55,85,125,165,200,225,235,255]
+
 c_list=[200]
 accuracylist=[]
 
 
 for C in c_list:
     test_results=Support_Vector_Machine(X_Train,Y_Train,X_Test,C,iterations,learning_rate,tolerance)
-
-
 
     summm=0
     summm2=0
@@ -263,21 +153,7 @@ for C in c_list:
             false_positive += 1
         elif (test_results[i] != Y_Test[i]) and (Y_Test[i] == -1):
             false_negative += 1
-            
-
-    #print(training_results)
-
-    #print(test_results)
-    #print(Y_Test)
-    #print(summm2/len(test_results))
-    #print(summm2/summm)
 
     accuracy = ((true_positive+true_negative)/len(test_results))
-
-    # print("False Negatives:",false_negative)
-    # print("False Positives:",false_positive)
-
-    # print("True Negatives:",true_negative)
-    # print("True Positives:",true_positive)
 
     print("Accuracy for c:",C,"   ",accuracy,"    ","True Positives:",true_positive,"True Negatives:",true_negative)
